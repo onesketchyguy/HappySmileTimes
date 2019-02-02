850962de2ca307bf7168087be302679a9ddc2681
DavidKozdra <davidkozdra@gmail.com>
2019-02-02 18:10:23 -0500

    da
    asdas

---
+++ FuckYouDavid/Assets/Scripts/ShopManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShopManager : MonoBehaviour
+{
+
+    public Player player;
+    public Text Costs;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {     
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+  
+
+}
