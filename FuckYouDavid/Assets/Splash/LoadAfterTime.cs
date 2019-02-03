48af15c3f3dda7685934a81be2cd14b3bb02d64e
DavidKozdra <davidkozdra@gmail.com>
2019-02-03 16:16:53 -0500

    Added new canvas to main and splash
    dasdas

---
+++ FuckYouDavid/Assets/Splash/LoadAfterTime.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAfterTime : MonoBehaviour
+{
+    [SerializeField] private float timeBeforeLoading = 3;
+
+    [SerializeField] private string sceneToLoad;
+
+    private void Start()
+    {
+        Invoke("Load", timeBeforeLoading);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Load();
+        }
+    }
+
+    private void Load()
+    {
+        if (sceneToLoad == "")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
