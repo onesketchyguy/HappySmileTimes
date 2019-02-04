1c7acecaccf8a475d614f08b8a36dc8aadfc08c4
DavidKozdra <davidkozdra@gmail.com>
2019-02-03 19:35:39 -0500

    bed
    bed

---
+++ FuckYouDavid/Assets/Scripts/BedManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+         Player player= collision.gameObject.GetComponent<Player>();
+         player.combattant.CurrentHealth = player.combattant.MaxHealth;
+        }
+       
+    }
+
+}
