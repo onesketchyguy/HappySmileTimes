bbd78820e8d4d29e177fe927bcbe24f7383fbcc4
onesketchyguy <34846360+onesketchyguy@users.noreply.github.com>
2019-02-05 15:40:46 -0800

    Added worlds

---
+++ FuckYouDavid/Assets/Scripts/Globals/Transition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Transition : MonoBehaviour
+{
+    public string LevelToLoad = "";
+
+    private string levelToLoad => LevelToLoad == "" ? "Main" : LevelToLoad;
+
+    private void Start()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //Bring up a travel dialogue
+            GameManager.instance.LoadScene(levelToLoad);
+        }
+    }
+}
